afbb286d287faf0483c321a9968beeb8f03be08a
nazwaputritania <nputritania@gmail.com>
2023-08-03 14:33:14 +0700

    tugas dart

---
+++ Documents/Tugas-ppbnazwa/1tugas.dart
@@ -0,0 +1,10 @@
+void main (List<String> args) {
+    var word = "dart";
+    var nazwa = " is";
+    var nazwa1 = " awesome";
+    var nazwa2 = " and";
+    var nazwa3 = " i";
+    var nazwa4 = " love";
+    var nazwa5 = " it!";
+    print (word + theresia + theresia1 + theresia2 + tehresia3 + theresia4 + theresia5);
+}
---
+++ Documents/Tugas-ppbnazwa/2tugas.dart
@@ -0,0 +1,19 @@
+void main (){
+    var kata = "i am going to be flutter developer";
+    var example_nazwa = kata[0];
+    var example1_nazwa = kata[2] + kata[3];
+    var example2_nazwa = kata[5] + kata[6] + kata[7] + kata[8] + kata[9];
+    var example3_nazwa = kata[11] + kata[12] + kata[13];
+    var example4_nazwa = kata[14] + kata[15];
+    var example5_nazwa = kata[17] + kata[18] + kata[19] + kata[20] + kata[21] + kata[22] + kata[23];
+    var example6_nazwa = kata[24] + kata[25] + kata[26] + kata[27] + kata[28] + kata[29] + kata[30] + kata[31] + kata[32] + kata[33];
+   
+   print('kata pertama:' + example_nazwa);
+    print('kata kedua:' + example1_nazwa);
+    print('kata ketiga:' + example2_nazwa);
+    print('kata keempat:' + example3_nazwa);
+    print('kata kelima:' + example4_nazwa);
+    print('kata keenam:' + example5_nazwa);
+    print('kata ketujuh:' + example6_nazwa);
+
+}
